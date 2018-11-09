b918e705b36591f1141560e69c62c3ea082654c6
taekeun <ytkkty@gmail.com>
2018-11-09 17:11:08 +0900

    author

---
+++ rails/many-to-many/app/controllers/authors_controller.rb
@@ -0,0 +1,25 @@
+class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+  end
+
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+
+    redirect_to @author
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name)
+  end
+end
